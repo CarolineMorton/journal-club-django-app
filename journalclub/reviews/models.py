54c99fed4980f114e870e1f199eea852047ce02a
CarolineMorton <carolinemorton@gmx.com>
2020-05-03 21:48:21 +0100

    correct model for articles

--- journalclub/reviews/models.py
+++ journalclub/reviews/models.py
@@ -1,17 +1,45 @@
 from django.db import models
 
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('Date Published')
+class Article_Review(models.Model):
+
+    Cohort = "COH"
+    CrossSectional = "CSS"
+    CaseControl = "CC"
+    RCT = "RCT"
+    Descriptive = "DSC"
+    Narrative = "NAR"
+    Review = "REV"
+
+    STUDY_CHOICES = [
+        (Cohort, "Cohort Study"),
+        (CrossSectional, "Cross-Sectional Study"), 
+        (CaseControl, "Case Control Study"), 
+        (RCT, "Randomised Clinical Trial"), 
+        (Descriptive, "Descriptive Study"),
+        (Narrative, "Editorial or Narrative"),
+        (Review, "Evidence Review") 
+    ]
+
+    citation = models.TextField()
+    paper_name = models.TextField()
+    author = models.CharField(max_length=1000)
+    year = models.CharField(max_length=200)
+    journal = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+
+    review_date = models.DateTimeField('Date Reviewed')
+
+    study_type = models.CharField(choices=STUDY_CHOICES, max_length=3)
+
+    clarity = models.TextField()
+
+    valid_methods = models.TextField()
+
+    results_importance = models.TextField()
+
+    generalisability = models.TextField()
+
+    take_home = models.TextField()
 
     def __str__(self):
-        return self.question_text
-
-
-class Answers(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer_test = models.CharField(max_length=1000)
-
-    def __str__(self):
-        return self.answer_test
+        return self.citation
