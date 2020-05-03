8247b0e0e0dfc59de3feb77b75b3b35f977e5ef3
CarolineMorton <carolinemorton@gmx.com>
2020-05-03 20:56:49 +0100

    adding basic structure of app, adding first couple of models

--- journalclub/reviews/models.py
+++ journalclub/reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Date Published')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Answers(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_test = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.answer_test
